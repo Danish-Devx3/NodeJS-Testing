a3ef59aa9edfa1424f6e21921204333322636d73
Danish-Devx3 <danish.devx3@gmail.com>
2025-08-19 15:26:46 +0530

    use vitest, deep mocking, mock resolve, spying, CICD

--- unit-test/src/tests/index.test.ts
+++ unit-test/src/tests/index.test.ts
@@ -1,18 +1,36 @@
 import { describe, expect, test, it, vi } from "vitest";
 import request from "supertest";
+import { prismaClient } from "../__mocks__/db";
 import { app } from "../index";
 
 // vi.mock("../db", () => ({
 //   prismaClient: { sum: { create: vi.fn() } },
 // }));
 
-vi.mock('../db.ts')
+vi.mock("../db.ts");
 
 describe("POST /sum", () => {
   it("should return the sum of two numbers", async () => {
+    prismaClient.sum.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 2,
+      result: 3,
+    });
+
+    vi.spyOn(prismaClient.sum, "create");
+    
+
     const res = await request(app).post("/sum").send({
       a: 1,
       b: 2,
+    });
+    expect(prismaClient.sum.create).toHaveBeenCalledWith({
+      data: {
+        a: 1,
+        b: 2,
+        result: 3,
+      },
     });
     expect(res.statusCode).toBe(200);
     expect(res.body.answer).toBe(3);
@@ -26,6 +44,23 @@ describe("POST /sum", () => {
 });
 
 describe("GET /sum", () => {
+  it("check mockResolved", async () => {
+    prismaClient.sum.create.mockResolvedValue({
+      id: 1,
+      a: 1,
+      b: 1,
+      result: 2,
+    });
+
+    const res = await prismaClient.sum.create();
+    expect(res).toEqual({
+      id: 1,
+      a: 1,
+      b: 1,
+      result: 2,
+    });
+  });
+
   it("should return the sum of two numbers", async () => {
     const res = await request(app)
       .get("/sum")
